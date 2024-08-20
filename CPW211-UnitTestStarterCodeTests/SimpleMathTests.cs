12012ca321f0455056c449a8287fbef743d00fb0
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-20 02:04:22 -0700

    coded and ran test for Add, subtract multiply SimpleMathTests

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -19,26 +19,47 @@ namespace CPW211_UnitTestStarterCode.Tests
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
             // Use the DataRow values to test the Add method
-            Assert.Fail();
+            double expected = num1 + num2; //get expected sum of two parameters
+            double actual = SimpleMath.Add(num1, num2); //call the method to get the sum
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(10, 10)]
+        [DataRow(1, 3)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
         {
             // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            double expected = num1 * num2; //expected parameters product
+            double actual = SimpleMath.Multiply(num1, num2); // actual method result
+            Assert.AreEqual(expected, actual);//Check if the results are equal
         }
 
         [TestMethod]
-        public void Divide_DenominatorZero_ThrowsArgumentException()
+        [DataRow(5, 0)]
+        [DataRow(10, 0)]
+        [DataRow(100, 0)]
+        public void Divide_DenominatorZero_ThrowsArgumentException(double num1, double num2)
         {
             // Divide by zero should throw an argument exception with a message
             // "Denominator cannot be zero"
-            Assert.Fail();
+            Assert.ThrowsException<ArgumentException>(() => SimpleMath.Divide(num1, num2));
         }
 
         // TODO: Test Divide method with two valid numbers
-
+        [TestMethod]
+        [DataRow(5, 5)]
+        [DataRow(10, 5)]
+        [DataRow(100, 5)]
+        [DataRow(20, 5)]
+        public void Divide_TwoNumbers_ReturnsQuotient(double num1, double num2)
+        {
+            double expected = num1 / num2;
+            double actual = SimpleMath.Divide(num1, num2);
+            Assert.AreEqual(expected, actual);
+        }
         // TODO: Test subtract method with two valid numbers
     }
 }
